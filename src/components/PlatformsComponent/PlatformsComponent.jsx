3e7429bf92ea1057c1439febb5db74c071449db7
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-06-26 18:42:45 +0530

    ui bugs fixed

--- src/components/PlatformsComponent/PlatformsComponent.jsx
+++ src/components/PlatformsComponent/PlatformsComponent.jsx
@@ -7,12 +7,12 @@ function PlatformsComponent() {
       <h2 className="platforms-heading">Integrate with multiple platforms</h2>
 
       <div className="platforms-logos">
-        <img src="/assets/platform/slide5.png" alt="Zoho CRM" />
-        <img src="/assets/platform/slide4.jpeg" alt="Shopify" />
-        <img src="/assets/platform/slide2.jpeg" alt="Google Sheets" />
-        <img src="/assets/platform/meta.jpeg" alt="Meta Business Suite" />
+        <img src="/assets/platform/zoho_crm.png" alt="Zoho CRM" />
+        <img src="/assets/platform/g_ads.png" alt="Google ads" />
+        <img src="/assets/platform/g_sheets.png" alt="Google Sheets" />
+        <img src="/assets/platform/meta.png" alt="Meta Business Suite" />
         <img src="/assets/platform/whatsapp.jpeg" alt="Whatsapp" />
-        <img src="/assets/platform/insta.jpeg" alt="Instagram" />
+        <img src="/assets/platform/insta.png" alt="Instagram" />
       </div>
 
       <a className="trial-button" href="#form" >Get Started for Free</a>
